74be2c00a67fc990f6fab9d58e2471b32cfaf60b
Nicolas Polomack <nicolas@polomack.eu>
2024-04-29 19:51:49 +0200

    refactor: renamed `forwardHeaders` to `filterHeaders`

--- src/routes/api/stream/+server.js
+++ src/routes/api/stream/+server.js
@@ -2,31 +2,31 @@ import process from 'node:process';
 
 /**
  * @param {Headers} headers
- * @param {string[]} forwardedHeaderNames
+ * @param {string[]} filteredHeaderNames
  * @returns {Object.<string, string>}
  */
-function forwardHeaders(headers, forwardedHeaderNames) {
+function filterHeaders(headers, filteredHeaderNames) {
     /** @type {Object.<string, string>} */
-    const forwardedHeaders = {};
-    for (const headerName of forwardedHeaderNames) {
+    const filteredHeaders = {};
+    for (const headerName of filteredHeaderNames) {
         const headerValue = headers.get(headerName);
         if (headerValue !== null) {
-            forwardedHeaders[headerName] = headerValue;
+            filteredHeaders[headerName] = headerValue;
         }
     }
-    return forwardedHeaders;
+    return filteredHeaders;
 }
 
 console.log('Process ID:', process.pid);
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET(event) {
-    const requestHeaders = forwardHeaders(event.request.headers, ['range']);
+    const requestHeaders = filterHeaders(event.request.headers, ['range']);
     const response = await fetch('https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4', {
         signal: event.request.signal,
         headers: requestHeaders,
     });
-    const responseHeaders = forwardHeaders(response.headers, ['content-length', 'content-type', 'content-range', 'accept-ranges']);
+    const responseHeaders = filterHeaders(response.headers, ['content-length', 'content-type', 'content-range', 'accept-ranges']);
     return new Response(response.body, {
         status: response.status,
         statusText: response.statusText,
